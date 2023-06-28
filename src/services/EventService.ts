439d2fbfdae983903a1232b71bbf2ee6c5e506ee
hoshizaki <kant.cmd@gmail.com>
2023-06-28 23:08:08 +0700

    (patch): add json-server

---
+++ src/services/EventService.ts
@@ -0,0 +1,18 @@
+// @ts-ignore
+import axios, {AxiosInstance, AxiosResponse} from 'axios'
+import type {EventItem} from "@/type";
+
+
+const apiClient: AxiosInstance = axios.create({
+    baseURL: 'http://localhost:3004',
+    withCredentials: false,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+const getEvent = (): Promise<AxiosResponse<EventItem[]>> => {
+    return apiClient.get<EventItem[]>('/events');
+}
+
+export default {getEvent}
