acb1dad6039292679ff2504d6e4e40df31fa72bf
hoshizaki <kant.cmd@gmail.com>
2023-06-28 16:58:46 +0700

    Init project

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink> |
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+<!--  TODO: 4.15-->
+  <RouterView />
+</template>
+
+<style scoped>
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
